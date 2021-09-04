d2360633918b210259af9cdc5856a53af8723123
David Grabosky <46197517+Azurize@users.noreply.github.com>
2021-09-04 08:19:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     client = MongoClient(os.environ.get("MONGODB_URI"))
-    app.db = client.microblog   # Actually connects to the DB
+    app.db = client.microblog
 
     @app.route("/", methods=["GET", "POST"])
     def home():
